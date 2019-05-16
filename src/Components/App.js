f1f43d724d2b1efe48212c3904ccd765f907ea21
yulyasystem <yulyasystem@gmail.com>
2019-05-16 16:01:47 +0300

    create navigation bar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+import Players from './Players';
+import Teams from './Teams';
+import Home from './Home';
+import Navbar from './Navbar';
 
 
 function App() {
   return (
-    <div className="App">
-      
-      
+    <Router>
+    <div>
+    <Navbar></Navbar>
+      <Route path = '/' exact component={Home}></Route>
+      <Route path = '/players' exact component={Players}></Route>
+      <Route path = '/teams' exact component={Teams}></Route>
     </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Players.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Players extends React.Component {
+  render() {
+
+    return (
+      <div className='container two-column'>
+        Players
+      </div>
+    );
+  }
+}
