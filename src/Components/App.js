f69e2ce9f9aa5445c7589c505d77fb96dc69ff19
yulyasystem <yulyasystem@gmail.com>
2019-05-16 16:06:41 +0300

    ad 404 catch

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Players from './Players';
 import Teams from './Teams';
 import Home from './Home';
 import Navbar from './Navbar';
 
-
 function App() {
   return (
     <Router>
-    <div>
-    <Navbar></Navbar>
-      <Route path = '/' exact component={Home}></Route>
-      <Route path = '/players' exact component={Players}></Route>
-      <Route path = '/teams' exact component={Teams}></Route>
-    </div>
+      <div>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/players' exact component={Players}></Route>
+          <Route path='/teams' exact component={Teams}></Route>
+          <Route render={()=><h1 className='text-center'> Four oh four lol :)</h1>}></Route>
+
+        </Switch>
+
+      </div>
     </Router>
   );
 }
